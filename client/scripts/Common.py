b8e7b6335694916c847de3a4966deb70b4412ec3
Pratyaksh Tyagi <tyagipratyaksh@gmail.com>
2021-02-14 00:15:50 +0530

    Minor Changes

--- client/scripts/Common.py
+++ client/scripts/Common.py
@@ -1,6 +1,6 @@
 def Common_Welcome(version_status):
     print(
-        f"Welcome to the BOT (Current plan - {version_status})  ! , please choose what you want to do."
+        f"Welcome to the BOT (Current plan - {version_status})  ! , Please choose what you want to do."
     )
     print("1. Check Settings")
     print("2. Start")
