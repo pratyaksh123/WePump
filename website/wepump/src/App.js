4771d8cae0d039c7667202ca1e11354f2315e93c
Pratyaksh Tyagi <tyagipratyaksh@gmail.com>
2021-02-21 01:37:02 +0530

    Remove that shitty library

--- website/wepump/src/App.js
+++ website/wepump/src/App.js
@@ -1,21 +1,37 @@
-import { FirestoreProvider, FirestoreCollection } from "@react-firebase/firestore";
-import { firebaseConfig } from "./firebaseConfig"
-import firebase from "firebase/app";
-import 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { db } from './services/Firestore'
 
 
 const App = () => {
+  const [users, setUsers] = useState([])
+  const fetchUsers = async () => {
+    const snapshot = await db.collection('users').get()
+    const temp = []
+    snapshot.docs.forEach((doc) => {
+      temp.push(doc.data())
+    })
+    setUsers(temp)
+  }
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
   return (
-    <FirestoreProvider {...firebaseConfig} firebase={firebase}>
-      <div>This is my app</div>
-      <div>
-        <FirestoreCollection path="/users/">
-
-          {d => <p>{JSON.stringify(d.value)}</p>}
-
-        </FirestoreCollection>
+    <div>
+      <h1 style={{ textAlign: "center" }}>Welcome to WePump Admin Panel</h1>
+      <div style={{ textAlign: "center" }}>
+        {
+          users && users.map((doc, idx) => {
+            return (
+              <div key={idx}>
+                <h4>{doc.email_id}</h4>
+                <p>{doc.status}</p>
+              </div>
+            )
+          })
+        }
       </div>
-    </FirestoreProvider>
+    </div>
   );
 };
 
